4f8e33f1c4f16d073dd91951dec79b48b64d15e6
Jyosolanki <118262106+Jyosolanki@users.noreply.github.com>
2023-03-11 21:51:37 +1000

    Excelclass added

--- ConsoleApp1/Utilities/ExcelData.cs
+++ ConsoleApp1/Utilities/ExcelData.cs
@@ -1,5 +1,4 @@
-﻿using Competition_task.Utilities;
-using ExcelDataReader;
+﻿using ExcelDataReader;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
@@ -10,34 +9,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Competition_task.Tests
+namespace Competition_task.Utilities
 {
     [TestFixture]
     public class ExcelDataTest
     {
-        [Test]  
+        [Test]
         public void ExcelReadDataForLoginCredentials()
         {
             //Path to the excel file with login credentials
-           var filePath = @"C:\Users\solan\Desktop\ProjMars\general\compotition task.exl.xlsx";
-          
+            var filePath = @"C:\Users\solan\Desktop\ProjMars\general\compotition task.exl.xlsx";
+
             //Encoding excel file stream
-            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-           
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
             //Reading login credentials from excel file to be used in login page
             var fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read);
-           var reader = ExcelReaderFactory.CreateOpenXmlReader(fileStream);
-           
+            var reader = ExcelReaderFactory.CreateOpenXmlReader(fileStream);
+
             //Getting the excel file as a dataset
             var dataSet = reader.AsDataSet();
 
             //Since only 1 sheet is in the excel file, index 0 is taken
 
             // for above give strings value
-           // dataTable = dataSet.Tables[0];
+            dataTable = dataSet.Tables[0];
 
-           // userName = dataTable.Rows[6][0].ToString();
-           // password = dataTable.Rows[6][1].ToString();
+            userName = dataTable.Rows[6][0].ToString();
+            password = dataTable.Rows[6][1].ToString();
         }
 
     }
